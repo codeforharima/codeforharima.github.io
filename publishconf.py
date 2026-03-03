02fe5c55e366f13ffdc0c0178f0ceca7a1217ce2
Jun Nogata <nogajun@gmail.com>
2026-03-03 11:07:21 +0900

    publishconf.pyのURLを修正

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "http://127.0.0.1:8000"
+SITEURL = "https://codeforharima.github.io"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
